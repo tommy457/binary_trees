3573bb7f99c196aa44ef8e42e4acb47e70829d18
tommy457 <boukhalfaml1011@gmail.com>
2023-10-31 03:51:35 -0700

    re-factor for task 15 to accommodate tree is null

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,5 +1,7 @@
 #include "binary_trees.h"
 
+int binary_tree_is_full_rec(const binary_tree_t *tree);
+
 /**
  * binary_tree_is_full - function that checks if a binary tree is full
  * @tree: pointer to the root node node of the tree to check.
@@ -10,15 +12,27 @@
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree)
+		return (binary_tree_is_full_rec(tree));
+	return (0);
+}
+
+/**
+ * binary_tree_is_full_rec - a recursive helper function.
+ * @tree: pointer to the root node node of the tree to check.
+ *
+ * Return: 1 or 0.
+ */
+
+int binary_tree_is_full_rec(const binary_tree_t *tree)
+{
+	if (tree)
 	{
 		if ((tree->left != NULL && tree->right == NULL) ||
-			(tree->left == NULL && tree->right != NULL) ||
-			!binary_tree_is_full(tree->left) ||
-			!binary_tree_is_full(tree->right)
+				(tree->left == NULL && tree->right != NULL) ||
+				binary_tree_is_full(tree->left) ||
+				binary_tree_is_full(tree->right)
 		   )
-			return (0);
+			return (1);
 	}
-	return (1);
-	else
-		return (0);
+	return (0);
 }
